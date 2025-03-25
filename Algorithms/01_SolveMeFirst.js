a21c3ff83c8cca9e14587601e471c5f2d6213c8a
alanbrunoscience <alanbrunomath@gmail.com>
2025-03-25 16:22:16 -0300

    :tada: Initial commit

--- Algorithms/01_SolveMeFirst.js
+++ Algorithms/01_SolveMeFirst.js
@@ -25,10 +25,10 @@ function solveMeFirst(a, b) {
 function main() {
 
     do {
-      var a = parseInt(readLine(), 10);
-      var b = parseInt(readLine(), 10);
+      var a = Number.parseInt(readLine(), 10);
+      var b = Number.parseInt(readLine(), 10);
     } while(isNaN(a) || isNaN(b) || a < 1 || a > 1000 || b < 1 || b > 1000);
 
     console.log(solveMeFirst(a, b));
-    
+
 }
