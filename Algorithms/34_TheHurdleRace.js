7164d40bf56638d2c6acad37025d6ffc7d6236cc
alanbrunoscience <alanbrunomath@gmail.com>
2025-05-08 19:46:41 -0300

    :sparkles: Include the 'The Hurdle Race' exercise solution

---
+++ Algorithms/34_TheHurdleRace.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', function () {
+  inputString = inputString.split('\n');
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+function hurdleRace(k, height) {
+  if (height.length === 0) return 0;
+
+  const maxHurdle = Math.max(...height);
+  return k >= maxHurdle ? 0 : maxHurdle - k;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+  const n = parseInt(firstMultipleInput[0], 10);
+
+  const k = parseInt(firstMultipleInput[1], 10);
+
+  const height = readLine().replace(/\s+$/g, '').split(' ').map(heightTemp => parseInt(heightTemp, 10));
+
+  const result = hurdleRace(k, height);
+
+  ws.write(result + '\n');
+
+  ws.end();
+}
